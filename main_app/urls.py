fcafabe8fee78e0f6fd92cc90fdf188aa5f5ec3c
Sean Yates <seanyates@Seans-MacBook-Pro.local>
2021-04-27 16:05:45 -0700

    adds the base.html and creates html file for each page on site

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('projects/', views.projects, name='projects'),
+    path('experience/', views.experience, name='experience'),
+    path('contact/', views.contact, name='contact'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def projects(request):
+    return render(request, 'projects.html')
+
+def experience(request):
+    return render(request, 'experience.html')
+
+def contact(request):
+    return render(request, 'contact.html')
